92d6365f42e5479475e64957869f2b9846c9d2d6
David Rosenberg <targendaz2@me.com>
2025-10-03 00:27:17 -0400

    Initial commit

---
+++ Helper/HelperProtocol.swift
@@ -0,0 +1,35 @@
+//
+//  HelperProtocol.swift
+//  Helper
+//
+//  Created by David Rosenberg on 10/3/25.
+//
+
+import Foundation
+
+/// The protocol that this service will vend as its API. This protocol will also need to be visible to the process hosting the service.
+@objc protocol HelperProtocol {
+    
+    /// Replace the API of this protocol with an API appropriate to the service you are vending.
+    func performCalculation(firstNumber: Int, secondNumber: Int, with reply: @escaping (Int) -> Void)
+}
+
+/*
+ To use the service from an application or other process, use NSXPCConnection to establish a connection to the service by doing something like this:
+
+     connectionToService = NSXPCConnection(serviceName: "com.dgrdev.UninstallAssistant.Helper")
+     connectionToService.remoteObjectInterface = NSXPCInterface(with: (any HelperProtocol).self)
+     connectionToService.resume()
+
+ Once you have a connection to the service, you can use it like this:
+
+     if let proxy = connectionToService.remoteObjectProxy as? HelperProtocol {
+         proxy.performCalculation(firstNumber: 23, secondNumber: 19) { result in
+             NSLog("Result of calculation is: \(result)")
+         }
+     }
+
+ And, when you are finished with the service, clean up the connection like this:
+
+     connectionToService.invalidate()
+*/
--- UninstallAssistant/ContentView.swift
+++ UninstallAssistant/ContentView.swift
@@ -5,17 +5,124 @@
 //  Created by David Rosenberg on 10/3/25.
 //
 
+import ServiceManagement
 import SwiftUI
 
 struct ContentView: View {
+    let helperServiceName = "com.dgrdev.UninstallAssistant.Helper"
+    var helperServicePlist: String {
+        helperServiceName + ".plist"
+    }
+
+    @State private var helperRegistered: Bool = false
+    @State private var helperApproved: Bool = false
+
+    @State private var testResult: Int?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("Helper Is Registered:")
+                Image(systemName: helperRegistered ? "checkmark.square.fill" : "x.square.fill")
+                    .foregroundColor(helperRegistered ? Color.green : Color.red)
+            }
+
+            HStack {
+                Text("Helper Is Approved:")
+                Image(systemName: helperApproved ? "checkmark.square.fill" : "x.square.fill")
+                    .foregroundColor(helperApproved ? Color.green : Color.red)
+            }
+
+            Button("Register Helper Daemon Service") {
+                print("Registering \(helperServicePlist)")
+                let service = SMAppService.daemon(plistName: helperServicePlist)
+
+                do {
+                    try service.register()
+                    print("Successfully registered \(service)")
+                    checkHelperStatus()
+                } catch {
+                    print("Unable to register \(error)")
+                }
+            }
+
+            Button("Unregister Helper Daemon Service") {
+                print("Unregistering \(helperServicePlist)")
+                let service = SMAppService.daemon(plistName: helperServicePlist)
+
+                do {
+                    try service.unregister()
+                    print("Successfully unregistered \(service)")
+                    helperRegistered = false
+                    helperApproved = false
+                    testResult = nil
+                } catch {
+                    print("Unable to unregister \(error)")
+                }
+            }
+
+            Button("Check Helper Status") {
+                checkHelperStatus()
+            }
+
+            if helperRegistered && helperApproved {
+                Button("Test Helper") {
+                    print("TEST: Sending Helper Request")
+
+                    let connection = NSXPCConnection(machServiceName: helperServiceName)
+                    print("TEST: Connection: \(connection)")
+
+                    connection.remoteObjectInterface = NSXPCInterface(with: HelperProtocol.self)
+                    connection.interruptionHandler = {
+                        print("TEST: XPC connection interrupted")
+                    }
+                    connection.invalidationHandler = {
+                        print("TEST: XPC connection invalidated")
+                    }
+                    connection.resume()
+                    print("TEST: Connection resumed")
+
+                    if let proxy = connection.remoteObjectProxy as? HelperProtocol {
+                        print("TEST: Proxy: \(proxy)")
+                        proxy.performCalculation(firstNumber: 23, secondNumber: 19) { result in
+                            print("Result of calculation is: \(result)")
+                            testResult = result
+                        }
+                        print("TEST: Done")
+                    }
+
+                    // connection.invalidate()
+                }
+            }
+
+            if testResult != nil {
+                Text("Test Result: \(testResult!)")
+            }
         }
         .padding()
+        .navigationTitle("LaunchDaemon Testing")
+        .onAppear {
+            checkHelperStatus()
+        }
+    }
+
+    func checkHelperStatus() {
+        print("Checking Helper Daemon Service Status")
+        let service = SMAppService.daemon(plistName: helperServicePlist)
+        print("Helper Daemon Service Status: \(service.status)")
+
+        if service.status == .enabled {
+            print("Helper Daemon Service is enabled")
+            helperRegistered = true
+
+            if service.status == .requiresApproval {
+                print("Helper Daemon Service requires approval")
+                SMAppService.openSystemSettingsLoginItems()
+            } else {
+                print("Helper Daemon Service is approved")
+                helperApproved = true
+            }
+        }
     }
 }
 
