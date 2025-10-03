350e1b376d97302f98b8e03170c39b8cd47f8e45
David Rosenberg <targendaz2@me.com>
2025-10-03 00:10:37 -0400

    Initial Commit

---
+++ UninstallAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UninstallAssistant
+//
+//  Created by David Rosenberg on 10/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UninstallAssistant/UninstallAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  UninstallAssistantApp.swift
+//  UninstallAssistant
+//
+//  Created by David Rosenberg on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct UninstallAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
